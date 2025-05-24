d59bc1c6b8dfd270b47fc3b88bdd696366924c16
cquinto <cristian.quinto0125@jala.university>
2025-05-24 10:12:09 -0300

    DTB-1: add connection provider

--- src/main/java/org/grupo3/MainApplication.java
+++ src/main/java/org/grupo3/MainApplication.java
@@ -1,11 +1,20 @@
 package org.grupo3;
 
+import org.grupo3.contexts.shared.infrastructure.ConnectionProvider;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  * Grupo-3-Todo-MySQL
  */
 public class MainApplication {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws SQLException {
+        Connection connection = ConnectionProvider.getConnection();
+
+        if (!(connection == null)) {
+            System.out.println("Successfully connected to MySQL database.");
+        }
     }
 
 }
